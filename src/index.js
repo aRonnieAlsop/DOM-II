c25185e0aa535f20c2b88d766cac24210d7efec7
Arron Alsop <alsop.arron@gmail.com>
2023-07-10 13:59:32 -0700

    'work in progress'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,89 @@
 import './less/index.less'
 
 // Your code goes here!
+/**
+ * 
+ *  Create listeners of 10 types of events
+ * * such as: 
+ *  [x] `mouseover` [x] `mouseout` [x]`click` [x]`copy`
+  * [x] `keydown`
+  * [ ] `wheel`
+  * [ ] `load`
+  * [ ] `focus`
+  * [ ] `resize`
+  * [ ] `scroll`
+  * [ ] `select`
+  * [x] `dblclick`
+  * [ ] `drag / drop`
+  * 
+  * Use preventDefault
+
+* [ ] Find a usecase for preventDefault. For example, you could prevent a 
+link from navigating when clicked, or to navigate somewhere surprising.
+ */
+
+
+const busImage = document.querySelector('.intro img');
+busImage.addEventListener('mouseover', event => {
+    busImage.style.filter = "invert(100%)";
+    
+})
+
+busImage.addEventListener('mouseout', event => {
+    busImage.style.filter = ""
+})
+
+const anotherPic = document.querySelector('.content-destination img')
+anotherPic.addEventListener('mouseover', event => {
+    anotherPic.style.filter = "invert(100%)";
+    
+})
+
+anotherPic.addEventListener('mouseout', event => {
+    anotherPic.style.filter = ""
+})
+
+const buttonList = document.querySelectorAll('.btn')
+buttonList[0].addEventListener('click', event => {
+    buttonList[0].style.backgroundColor = 'hotPink';
+})
+buttonList[1].addEventListener('click', event => {
+    buttonList[1].style.backgroundColor = 'hotPink';
+})
+buttonList[2].addEventListener('click', event => {
+    buttonList[2].style.backgroundColor = 'hotPink';
+})
+
+const navBarItem = document.querySelectorAll('.nav-link')
+navBarItem[0].addEventListener('dblclick', event => {
+    alert("Please wait. Page Under Construction");
+    event.preventDefault();
+})
+navBarItem[1].addEventListener('dblclick', event => {
+    alert("Please wait. Page Under Construction");
+    event.preventDefault();
+})
+navBarItem[2].addEventListener('dblclick', event => {
+    alert("Please wait. Page Under Construction");
+    event.preventDefault();
+})
+navBarItem[3].addEventListener('dblclick', event => {
+    alert("Contact Not Available");
+    event.preventDefault();
+})
+
+document.addEventListener("copy", event => {
+    alert("this page is Copyrighted--use content at own risk");
+ })      
+
+
+ document.addEventListener("keydown", event => {
+    console.log(event)
+    if(event.keyCode === 70) {
+        document.body.style.backgroundColor = 'hotPink'
+    }
+ })
+ const imageMap = document.querySelector('.content-section .img-content img');
+ const imageVenice = document.querySelector('.inverse-content .img-content img');
+
+ 
